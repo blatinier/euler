c585f3ca711cbe1e97b72685403edfc6ec8244b4
Benoit Latinier <benoit.latinier@durable.com>
2013-04-17 21:33:49 +0200

    add: some more functions

--- euler.py
+++ euler.py
@@ -50,14 +50,54 @@ def factors(n):
         if n % i == 0:
             l.append(i)
     return l
-b,l = 1,500000000
-for i in xrange(b,l):
-    ti = triangle_num(i)
-    fti = factors(ti)
-    if len(fti) >= 500:
-        print i
-        print ti
-        print len(fti)
-        break
-    if i % 100000 == 0:
-        print i
+
+def int2word(n):
+    ones = ["", "one ","two ","three ","four ", "five ",
+    "six ","seven ","eight ","nine "]
+    tens = ["ten ","eleven ","twelve ","thirteen ", "fourteen ",
+    "fifteen ","sixteen ","seventeen ","eighteen ","nineteen "]
+    twenties = ["","","twenty ","thirty ","forty ",
+    "fifty ","sixty ","seventy ","eighty ","ninety "]
+    thousands = ["","thousand "]
+    n3 = []
+    r1 = ""
+    ns = str(n)
+    for k in range(3, 33, 3):
+        r = ns[-k:]
+        q = len(ns) - k
+        if q < -2:
+            break
+        else:
+            if q >= 0:
+                n3.append(int(r[:3]))
+            elif q >= -1:
+                n3.append(int(r[:2]))
+            elif q >= -2:
+                n3.append(int(r[:1]))
+        r1 = r
+    nw = ""
+    for i, x in enumerate(n3):
+        b1 = x % 10
+        b2 = (x % 100)//10
+        b3 = (x % 1000)//100
+        if x == 0:
+            continue # skip
+        else:
+            t = thousands[i]
+        if b2 == 0:
+            nw = ones[b1] + t + nw
+        elif b2 == 1:
+            nw = tens[b1] + t + nw
+        elif b2 > 1:
+            nw = twenties[b2] + ones[b1] + t + nw
+        if b3 > 0:
+            nw = ones[b3] + "hundred and " + nw
+    return nw
+
+def is_amicable(n):
+    f = sum(factors(n)) - n
+    f2 = sum(factors(f)) - f
+    return f == f2
+
+print sum([i for i in xrange(10000) if is_amicable(i)])
+    
