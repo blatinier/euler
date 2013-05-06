34a2fab8701a4ada4ca5ba5ba66d0accbea7df6f
Benoit Latinier <benoit.latinier@durable.com>
2013-05-06 22:46:42 +0200

    add: method to compute sqrt digits by digits

--- euler.py
+++ euler.py
@@ -673,6 +673,39 @@ def apply_pattern(sn, pat, k):
         return False
     return int(nd)
 
+def sqrt_list(n, precision):
+    """Compute the square root to require precision and return
+    the digits list"""
+    ndigits = []        # break n into list of digits
+    n_int = int(n)
+    n_fraction = n - n_int
+
+    while n_int:                            # generate list of digits of integral part
+        ndigits.append(n_int % 10)
+        n_int /= 10
+    if len(ndigits) % 2: ndigits.append(0)  # ndigits will be processed in groups of 2
+
+    decimal_point_index = len(ndigits) / 2  # remember decimal point position
+    while n_fraction:                       # insert digits from fractional part
+        n_fraction *= 10
+        ndigits.insert(0, int(n_fraction))
+        n_fraction -= int(n_fraction)
+    if len(ndigits) % 2: ndigits.insert(0, 0)  # ndigits will be processed in groups of 2
+
+    rootlist = []
+    root = carry = 0                        # the algorithm
+    # TODO compute the len inside loop to save the use of len
+    while root == 0 or (len(rootlist) < precision and (ndigits or carry != 0)):
+        carry = carry * 100
+        if ndigits: carry += ndigits.pop() * 10 + ndigits.pop()
+        x = 9
+        while (20 * root + x) * x > carry:
+                x -= 1
+        carry -= (20 * root + x) * x
+        root = root * 10 + x
+        rootlist.append(x)
+    return rootlist, decimal_point_index
+
 def is_permutation(n, k):
     """Return True if k is a permutation of n"""
     return sorted(str(n)) == sorted(str(k))
@@ -816,6 +849,14 @@ def fractran(seed, fracts):
 #print cnt
 #exit()
 #print "Problem 75"
+
+# Problem 80
+print "Problem 80"
+for i in xrange(1, 101):
+    s = sqrt(i)
+    if not s.is_integer():
+        print "%s is irrational" % s 
+print "Problem 80"
 
 #print "Problem 94"
 #def area_is_int(a, b):
