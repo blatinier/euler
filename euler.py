fe3f7a9ac5e438c72d1ca31dafaaf8dbd4464385
Benoit Latinier <benoit.latinier@durable.com>
2013-05-06 23:31:03 +0200

    comments

--- euler.py
+++ euler.py
@@ -673,17 +673,19 @@ def apply_pattern(sn, pat, k):
         return False
     return int(nd)
 
+# TODO division from future is wrong on it!
 def sqrt_list(n, precision):
     """Compute the square root to require precision and return
     the digits list"""
-    ndigits = []        # break n into list of digits
+    ndigits = []  # break n into list of digits
     n_int = int(n)
     n_fraction = n - n_int
 
-    while n_int:                            # generate list of digits of integral part
+    while n_int:  # generate list of digits of integral part
         ndigits.append(n_int % 10)
         n_int /= 10
-    if len(ndigits) % 2: ndigits.append(0)  # ndigits will be processed in groups of 2
+    if len(ndigits) % 2:
+        ndigits.append(0)  # ndigits will be processed in groups of 2
 
     decimal_point_index = len(ndigits) / 2  # remember decimal point position
     while n_fraction:                       # insert digits from fractional part
@@ -694,13 +696,13 @@ def sqrt_list(n, precision):
 
     rootlist = []
     root = carry = 0                        # the algorithm
-    # TODO compute the len inside loop to save the use of len
     while root == 0 or (len(rootlist) < precision and (ndigits or carry != 0)):
         carry = carry * 100
-        if ndigits: carry += ndigits.pop() * 10 + ndigits.pop()
+        if ndigits:
+            carry += ndigits.pop() * 10 + ndigits.pop()
         x = 9
         while (20 * root + x) * x > carry:
-                x -= 1
+            x -= 1
         carry -= (20 * root + x) * x
         root = root * 10 + x
         rootlist.append(x)
