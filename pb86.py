ce46da7437c11c9f616af80cfe29ee71e23281d7
Benoit Latinier <benoit.latinier@durable.com>
2013-11-08 08:10:48 +0100

    optim 86

--- pb86.py
+++ pb86.py
@@ -12,18 +12,10 @@ limit = 2000
 M = 0
 nb_sol = 0
 while nb_sol < limit:
-    print M, nb_sol
     M += 1
-    nb_sol = 0
-    for a in xrange(1, M+1):
-        for b in xrange(1, a+1):
-            for c in xrange(1, b+1):
-                if shortest_path_is_int(a, b, c):
-                    nb_sol += 1
-#                    if a == b == c:
-#                        nb_sol += 1
-#                    elif a == b or b == c or a == c:
-#                        nb_sol += 3
-#                    else:
-#                        nb_sol += 6
+    for b in xrange(1, M+1):
+        for c in xrange(1, b+1):
+            if shortest_path_is_int(M, b, c):
+                nb_sol += 1
+    print M, nb_sol
 print M, nb_sol
