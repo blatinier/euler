5783451cc341258c20a285219a42c31abb37aa54
Benoit Latinier <benoit.latinier@durable.com>
2014-09-02 15:13:29 +0200

    add others triangles

--- pb94.py
+++ pb94.py
@@ -4,10 +4,13 @@ from euler import progress
 cpt = 0
 L = 10 ** 9 / 3
 for i in xrange(1, L, 2):
-    progress(i, L, 1)
+    progress(i, L, 100001)
     s = sqrt((3*i+1)*(i-1))
+    s2 = sqrt((3*i-1)*(i+1))
     if int(s) == s and (s % 4 == 0):
         cpt += 3 * i + 1
+    if int(s2) == s2 and (s2 % 4 == 0):
+        cpt += 3 * i - 1
 print "WINNER"
 print cpt
 print "WINNER"
@@ -26,3 +29,8 @@ print "WINNER"
     #<=> (i % 2 == 1) && (sqrt((i + (i+1)/2)*(i - (i+1)/2)) % 2 == 0)
     #<=> (i % 2 == 1) && (sqrt((3*i+1)/2*(i+1)/2) % 2 == 0)
     #<=> (i % 2 == 1) && (sqrt((3*i+1)*(i-1)) % 4 == 0)
+
+    #<=> (i % 2 == 1) && (sqrt((i+a/2)*(i-a/2)) % 2 == 0)
+    #<=> (i % 2 == 1) && (sqrt((i + (i-1)/2)*(i - (i-1)/2)) % 2 == 0)
+    #<=> (i % 2 == 1) && (sqrt((3*i-1)/2*(i+1)/2) % 2 == 0)
+    #<=> (i % 2 == 1) && (sqrt((3*i-1)*(i+1)) % 4 == 0)
